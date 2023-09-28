a4a55a36455a5d2fee0bb90f3f9081515696bc16
Benngki <benediktus.setiawan@binus.ac.id>
2023-09-29 01:46:24 +0700

    fix nav link

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ $('nav a').click(function (e) {
 
     var href = $(this).attr("href");
     $("html, body").animate({ scrollTop: $(href).offset().top - window.innerHeight *0.1}, 300);
+});    
+
+$(document).ready(function () {
+    var section = window.location.href.split('index.html')[1];
+    if (section) {
+        $(`nav a[href='${section}']`).click();            
+    } else {
+        $(`nav a[href='#home']`).click();
+    }
 });
