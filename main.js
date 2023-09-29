a774e314fcc473ee3ef1000ab6724c171c8fe522
Benngki <benediktus.setiawan@binus.ac.id>
2023-09-29 16:48:15 +0700

    enable dark mode

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 var navHeight = $('nav').outerHeight();
 
+function changeMode(mode) {
+    if (mode === 'dark') {
+        $('html').attr('data-bs-theme', 'dark');
+        if ($('#toggle_switch .light_btn').hasClass('d-none')) {
+            $('#toggle_switch .light_btn').removeClass('d-none');
+        }
+        else {
+            $('#toggle_switch .dark_btn').removeClass('d-none')
+            $('#toggle_switch .light_btn').addClass('d-none');
+        }
+    } else {
+        $('html').attr('data-bs-theme', '');     
+        if ($('#toggle_switch .dark_btn').hasClass('d-none')) {
+            $('#toggle_switch .dark_btn').removeClass('d-none');
+        }
+        else {
+            $('#toggle_switch .light_btn').removeClass('d-none')
+            $('#toggle_switch .dark_btn').addClass('d-none');
+        } 
+    }
+    
+}
+
 $(document).ready(function () {    
     $("main").css('margin-top', navHeight);
 });
